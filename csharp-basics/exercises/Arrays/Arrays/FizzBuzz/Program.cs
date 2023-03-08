2265e4d4d2fba26913d56a9af3a73c4a9296de75
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-08 19:51:14 +0200

    FizzBuzz

---
+++ csharp-basics/exercises/Arrays/Arrays/FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FizzBuzz;
+class FizzBuzz
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter an integer: ");
+        int number = int.Parse(Console.ReadLine()!);
+
+        for (int index = 1; index <= number; index++)
+        {
+            if (index % 3 == 0 && index % 5 == 0)
+            {
+                Console.Write("FizzBuzz ");
+            }
+            else if (index % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else if (index % 5 == 0)
+            {
+                Console.Write("Buzz ");
+            }
+            else
+            {
+                Console.Write(index + " ");
+            }
+
+            if (index % 20 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
