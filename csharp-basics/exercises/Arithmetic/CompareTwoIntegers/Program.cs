bded829274a964b5bf74bf943e656bfccd300e33
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-08 13:58:54 +0200

    CompareIntegers

---
+++ csharp-basics/exercises/Arithmetic/CompareTwoIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CompareTwoIntegers;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter first number: ");
+        var first = int.Parse(Console.ReadLine()!);
+        Console.Write("Please enter second number: ");
+        var second = int.Parse(Console.ReadLine()!);
+
+        var result = Compare(first, second);
+        Console.WriteLine("Input numbers generate result -> {0}", result);
+    }
+
+    static bool Compare(int first, int second)
+    {
+        var add = first + second;
+        var difference = first - second;
+        if (first == 15 || second == 15)
+        {
+            return true;
+        } else if(add == 15 || difference == 15)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
