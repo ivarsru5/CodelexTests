428342b3a7f7c2794ca2b517b5db744db80e2afc
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-08 17:08:36 +0200

    GetTheCentury fix

--- csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
@@ -6,7 +6,14 @@ namespace GetTheCentury
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the year: ");
+            var year = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("The century for year {0} is {1}", year, GetCentury(year));
+        }
 
+        static int GetCentury(int year)
+        {
+            return (year % 100 == 0) ? (year / 100) : (year / 100 + 1);
         }
     }
 }
