335f2e5ded601f978d117178a67cda7675daf112
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-09 11:50:17 +0200

    Find nemo task

---
+++ csharp-basics/exercises/Arrays/Arrays/FindNemo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FindNemo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FindNemoFunc("I am Nemo and dont know anything !"));
+    }
+
+    static string FindNemoFunc(string input)
+    {
+        var words = input.Split(' ');
+        var result = "";
+        for (var index = 0; index < words.Length; index++)
+        {
+            if (words[index] == "Nemo")
+            {
+                result = "I found Nemo at " + index;
+                break;
+            }
+            else
+            {
+                result = "I cant find nemo";
+            }
+        }
+        return result;
+    }
+}
+
