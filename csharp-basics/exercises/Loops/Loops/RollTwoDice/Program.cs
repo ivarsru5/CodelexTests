3d649093dba26db3a0426804e23dc3557caa993a
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-09 13:32:03 +0200

    RollTwoDice task

---
+++ csharp-basics/exercises/Loops/Loops/RollTwoDice/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RollTwoDice;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter desired summ: ");
+        var summ = int.Parse(Console.ReadLine()!);
+        var diceOne = 0;
+        var diceTwo = 0;
+        RollDice(summ, diceOne, diceTwo);
+    }
+
+    static void RollDice(int summ, int diceOne, int diceTwo)
+    {
+        while ((diceOne + diceTwo) != summ)
+        {
+            Random roll = new Random();
+            diceOne = roll.Next(1,7);
+            diceTwo = roll.Next(1,7);
+            Console.WriteLine("{0} and {1} = {2}", diceOne, diceTwo, diceTwo+diceOne);
+        }
+    }
+}
+
