3bac01d4440b168aaa6cba418280b3631089138f
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-09 21:35:21 +0200

    Savings account

---
+++ csharp-basics/exercises/ClassesAndObjects/SavingsAccount/SavingsAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace SavingsAccount
+{
+	public class SavingsAccount
+	{
+		private double _savingsAmount;
+		public double totalInInterests;
+		public double totalWidraw;
+		public double totalDeposit;
+
+		public SavingsAccount(double savings)
+		{
+			this._savingsAmount = savings;
+		}
+
+        public void Widraw(double amount)
+        {
+			 this._savingsAmount -= amount;
+			totalWidraw += amount;
+        }
+
+		public void AddMoney(double amount)
+		{
+			this._savingsAmount += amount;
+			totalDeposit += amount;
+
+        }
+
+		public void Interest(double interestRate)
+		{
+			var percent = interestRate / 100;
+			var interest = _savingsAmount * percent;
+			var eachMonth = interest / 12;
+			totalInInterests += eachMonth;
+			_savingsAmount += eachMonth;
+		}
+
+		public double GetBalance()
+		{
+			return _savingsAmount;
+		}
+    }
+}
+
