9ada8b5af350bf1c79b53a04367cf1c56cd1f407
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-09 11:41:54 +0200

    Array element counting

---
+++ csharp-basics/exercises/Arrays/Arrays/ArrayElementCounting/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ArrayElementCounting;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 };
+        var result = CountElements(numArray);
+        Console.WriteLine(string.Join(",", result));
+    }
+
+    static List<int> CountElements(int[] intArray)
+    {
+        List<int> completed = new List<int>();
+        int positive = 0;
+        int negative = 0;
+        foreach (var element in intArray)
+        {
+            if (element < 0)
+            {
+                negative += element;
+            }
+            else
+            {
+                positive += 1;
+            }
+        }
+        completed.Add(positive);
+        completed.Add(negative);
+        return completed;
+    }
+}
+
