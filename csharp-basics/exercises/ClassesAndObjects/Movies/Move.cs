f519f60f02fecb08937202c78fd0ff1c62b1d660
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-09 15:52:31 +0200

    Movies object

---
+++ csharp-basics/exercises/ClassesAndObjects/Movies/Move.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Movies
+{
+	public class Move
+	{
+		public string Title;
+		public string Studio;
+		public string Rating;
+
+		public Move(string title, string studio, string rating)
+		{
+			this.Title = title;
+			this.Studio = studio;
+			this.Rating = rating;
+		}
+
+		public Move(string title, string studio)
+		{
+			this.Title = title;
+			this.Studio = studio;
+			this.Rating = "PG";
+		}
+	}
+}
+
