34a02f142ea7bc7a18506c7eabc3c34cdc6d1161
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-10 08:10:38 +0200

    Points

---
+++ csharp-basics/exercises/ClassesAndObjects/Points/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Points;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var p1 = new Points(-1, -2);
+        var p2 = new Points(1, 2);
+        Points.SwitchPoints(ref p1, ref p2);
+
+        Console.WriteLine("x = {0}, y = {1}", p1.x, p1.y);
+    }
+}
