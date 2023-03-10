df16746c123eb8e135403904ce2e8e5c063cceaf
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-10 10:31:39 +0200

    Account transfers

--- csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -7,17 +7,19 @@
 
         public Account(string v1, double v2)
         {
-            
+            this._name = v1;
+            this._money = v2;
         }
 
         public double Withdrawal(double i)
         {
+            _money -= i;
             return i;
         }
 
         public void Deposit(double i)
         {
-            
+            _money += i;
         }
 
         public double Balance()
@@ -28,6 +30,12 @@
         public override string ToString()
         {
             return $"{_name}: {_money}";
+        }
+
+        public static void Transfer(Account from, Account to, double summ)
+        {
+            var amount = from.Withdrawal(summ);
+            to.Deposit(amount);
         }
 
         public string Name
--- csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -10,6 +10,18 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            var account = new Account("Barto's", 100);
+            Console.WriteLine("Account: {0}", account.ToString());
+            account.Deposit(20);
+            Console.WriteLine("Account after deposit: {0}", account.ToString());
+
+            var matAccount = new Account("Mat", 1000);
+            var myAccount = new Account("MyAccount", 0);
+
+            Account.Transfer(matAccount, myAccount, 100);
+
+            Console.WriteLine("Account: {0}", matAccount.ToString());
+            Console.WriteLine("Account: {0}", myAccount.ToString());
         }
     }
 }
