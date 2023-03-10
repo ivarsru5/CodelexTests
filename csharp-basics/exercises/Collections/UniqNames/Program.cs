627b70eba838be5f24405e5d667bce595eb5f3b1
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-10 11:26:20 +0200

    Uniq names

---
+++ csharp-basics/exercises/Collections/UniqNames/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UniqNames;
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> names = new HashSet<string>();
+
+        while (true)
+        {
+            Console.Write("Enter name: ");
+            var name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+                break;
+
+            names.Add(name!);
+        }
+        foreach (var name in names)
+            Console.Write($"{name}, ");
+    }
+}
+
