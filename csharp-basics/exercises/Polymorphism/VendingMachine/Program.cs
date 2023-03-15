493b15230ec5dff08276f8228a9e563bbd95458f
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2023-03-15 16:39:46 +0200

    Vending machine

--- csharp-basics/exercises/Polymorphism/VendingMachine/Program.cs
+++ csharp-basics/exercises/Polymorphism/VendingMachine/Program.cs
@@ -6,7 +6,102 @@ namespace VendingMachine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please insert coins to buy something");
+
+            var insert = true;
+            var summ = new Money();
+            var vendingMachine = new VendingMachine("Bosch");
+
+            while (insert)
+            {
+                Console.Write("Insert: ");
+                var coin = Convert.ToDouble(Console.ReadLine());
+
+                var coinValue = ValidateCoin(coin);
+                var coinValidator = coinValue.Euros < 0 || coinValue.Cents < 0;
+
+                while (coinValidator == true)
+                {
+                    Console.WriteLine("Coin is not valid, please try again!");
+                    coin = Convert.ToDouble(Console.ReadLine());
+                }
+
+                summ = vendingMachine.InsertCoin(coinValue);
+                var balance = summ.Euros + (summ.Cents * 1.0 / 100);
+
+                Console.WriteLine("Available balance: {0}", balance);
+
+                Console.Write("Display products {0}, or insert coin{1}", 2, 1);
+                var select = Convert.ToDouble(Console.ReadLine());
+
+                if (select == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    insert = false;
+                }
+
+            }
+
+            var selected = SelectProduct(vendingMachine.Products);
+            if (selected != null)
+            {
+                vendingMachine.ProcessDeal((Product)selected);
+                Console.WriteLine("Deal done successfuly");
+                var change = vendingMachine.Amount.Euros + (vendingMachine.Amount.Cents * 1.0 / 100);
+                Console.WriteLine("Change: {0}", change);
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+        }
+
+        public static Money ValidateCoin(double coin)
+        {
+            Money insertedCoin = new Money();
+            switch (coin)
+            {
+                case 0.10:
+                    insertedCoin = new Money { Euros = 0, Cents = 10 };
+                    break;
+                case 0.20:
+                    insertedCoin = new Money { Euros = 0, Cents = 20 };
+                    break;
+                case 0.50:
+                    insertedCoin = new Money { Euros = 0, Cents = 50 };
+                    break;
+                case 1.00:
+                    insertedCoin = new Money { Euros = 1, Cents = 0 };
+                    break;
+                case 2.00:
+                    insertedCoin = new Money { Euros = 2, Cents = 0 };
+                    break;
+                default:
+                    break;
+            }
+            return insertedCoin;
+        }
+
+        public static Product? SelectProduct(Product[] products)
+        {
+            for (var index = 0; index < products.Length; index++)
+            {
+                Console.WriteLine("{0}: {1} price: {2}", index + 1, products[index].Name, products[index].Price);
+            }
+            Console.Write("Select product: ");
+            var productIndex = Convert.ToInt32(Console.ReadLine());
+
+            if (productIndex >= 0 && productIndex <= products.Length)
+            {
+                return products[productIndex - 1];
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
---
+++ csharp-basics/exercises/Polymorphism/VendingMachine/VendingMachine.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace VendingMachine
+{
+    public class VendingMachine : IVendingMachine
+    {
+        private string _manufacturer;
+        private Product[] _products;
+        private Money _amount;
+
+        public VendingMachine(string manufacturer)
+        {
+            _manufacturer = manufacturer;
+            _amount = new Money();
+            _products = new Product[]
+            {
+                new Product
+                {
+                    Available = 10,
+                    Price = new Money
+                    {
+                        Euros = 1,
+                        Cents = 25
+                    },
+                    Name = "Patao chips"
+                },
+
+                new Product
+                {
+                    Available = 11,
+                    Price = new Money
+                    {
+                        Euros = 0,
+                        Cents = 75
+                    },
+                    Name = "Coca-Cola can"
+                }
+            };
+        }
+
+        public string Manufacturer
+        {
+            get { return _manufacturer; }
+        }
+
+        public bool HasProducts
+        {
+            get { return _products.Length > 0; }
+        }
+
+        public Money Amount
+        {
+            get { return _amount; }
+        }
+
+        public Product[] Products
+        {
+            get { return _products; }
+            set { _products = value; }
+        }
+
+        public Money InsertCoin(Money amount)
+        {
+            var balance = _amount.Euros + (_amount.Cents * 1.0 / 100);
+            var inserted = amount.Euros + (amount.Cents * 1.0 / 100);
+
+            var newBalance = balance + inserted;
+
+            var euros = (int)newBalance;
+            var cents = (int)((newBalance - euros) * 100);
+
+            _amount.Euros = euros;
+            _amount.Cents = cents;
+
+            return Amount; 
+        }
+
+        public Money ReturnMoney()
+        {
+            return _amount;
+        }
+
+        public bool AddProduct(string name, Money price, int count)
+        {
+            var newPrice = price.Euros + (price.Cents * 1.0 / 100);
+
+            if (!string.IsNullOrEmpty(name) && newPrice != 0.00 && count != 0)
+            {
+                var product = new Product
+                {
+                    Available = count,
+                    Price = price,
+                    Name = name
+                };
+                var index = _products.Length;
+                _products[index] = product;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateProduct(int productNumber, string name, Money? price, int amount)
+        {
+            if (productNumber >= 0 && productNumber < _products.Length && price.HasValue)
+            {
+                var product = _products[productNumber - 1];
+                product.Name = name;
+                product.Price = (Money)price;
+                product.Available = amount;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void ProcessDeal(Product selectedProduct)
+        {
+            var balance = _amount.Euros + (_amount.Cents * 1.0 / 100);
+            var productPrice = selectedProduct.Price.Euros! + (selectedProduct.Price.Cents * 1.0 / 100);
+            var remainingBalance = balance - productPrice;
+
+            var euro = (int)remainingBalance;
+            var cents = (int)((remainingBalance - euro) * 100);
+            _amount.Euros = euro;
+            _amount.Cents = cents;
+        }
+    }
+}
+
+
